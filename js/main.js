6f95f5917f364529d03c0477955e532f538748da
blueberry20 <olgasales@gmail.com>
2016-01-10 17:15:12 -0500

    changed gulp commands, cleaned up code

--- js/main.js
+++ js/main.js
@@ -2,17 +2,9 @@
 		var headingChanged = false;
 		var randomFontObject;
 		var randomFontObject2;
+		var refreshCount = 0;
 		$(document).ready(function(){
 			randomizeAllFonts();
-
-			//$('#sidebar').height($(window).height());
-
-			// document.getElementById("heading").addEventListener("input", function() {
-			// 	alert('dfdf');
-			//     var value = document.getElementById("heading").value;
-			//     document.getElementById("heading").innerHtml = value;
-			//     headingChanged = true;
-			// }, false);
 		});
 
 		function randomizeAllFonts(){
@@ -20,7 +12,7 @@
 		}
 
 		function loadFont(heading, paragraph, variantChangeClicked){
-
+			refreshCount++;
 			$.ajax({ 
 			    type: 'GET', 
 			    url: 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDdklKz1qg_GvG_9ZjLL47pOq_1TdP5oD0', 
@@ -30,21 +22,15 @@
 			    	var fontsArray = data.items;
 			    	randomFontObject = fontsArray[Math.floor(Math.random() * fontsArray.length)];
 			    	randomFontObject2 = fontsArray[Math.floor(Math.random() * fontsArray.length)];
-			    	// console.log(randomFontObject);
-			    	// console.log(randomFontObject2);
 
 			    	if (heading && paragraph) {
-						//changeFont(randomFontIndex.family, randomFontIndex2.family);
-						//$('#heading, #paragraph').css('opacity', 0);
 						randomizeHeading(randomFontObject);
 						randomizeParagraph(randomFontObject2);
 					}
 					else if (heading && !paragraph) {
-						//$('#heading').css('opacity', 0);
 						randomizeHeading(randomFontObject);
 					}
 					else if (!heading && paragraph) {
-						//$('#paragraph').css('opacity', 0);
 						randomizeParagraph(randomFontObject2);
 					}
 			    },
@@ -56,7 +42,7 @@
 
 
 		function randomizeHeading(fontObject){
-			$('#heading').removeClass('loaded');
+			
 			var fontName = fontObject.family;
 			fontNameForUrl = fontName.replace(/\s+/g, '+');
 		    WebFont.load({
@@ -64,21 +50,39 @@
 	                   families: [fontNameForUrl] 
 	                } 
 			 }); 
-		    setTimeout(function(){
-		    	$('#heading').css({'font-family': fontName }).addClass('loaded');
-		    }, 500)
-			
-			$('#headingFontInfo').html('<a target="_blank" href="https://www.google.com/fonts/specimen/'+ fontNameForUrl +'">' + fontName + '</a>');
-
-			var fontVariants = '';
+		    var fontVariants = '';
 			for (var i = 0; i < fontObject.variants.length; i++) {
 				fontVariants += '<span onclick="useThisFontVariant(this, \'heading\');">' + fontObject.variants[i] + '</span>';
 			}
-			$('#headingFontVariants').html(fontVariants);
+		    $('#heading').removeClass('fadeIn');
+		    if (refreshCount !=1){
+		    	$('.headingInfoWrapper.current').addClass('slideDown');
+
+				$('#headingInfo').append(
+					'<div class="headingInfoWrapper infoWrapper next">'+
+						'<h2>Heading</h2>'+
+						'<div class="divider"></div>'+
+						'<span id="headingFontInfo"><a target="_blank" href="https://www.google.com/fonts/specimen/'+ fontNameForUrl +'">' + fontName + '</a></span>'+
+						'<h3>Font Variants:</h3>'+
+						'<span id="headingFontVariants">'+fontVariants+'</span>'+
+						'<button id="randomizeHeading" class="randomizeBtn" onclick="loadFont(true, false)";><i class="fa fa-refresh"></i></button>'+
+					'</div>'
+					);
+			    setTimeout(function(){
+			    	$('.headingInfoWrapper.current').remove();
+					$('.headingInfoWrapper.next').removeClass('next').addClass('current');
+			    	$('#heading').css({'font-family': fontName }).addClass('fadeIn');
+			    }, 500)
+			}
+			else {
+				$('#headingFontInfo').html('<a target="_blank" href="https://www.google.com/fonts/specimen/'+ fontNameForUrl +'">' + fontName + '</a>');
+				$('#headingFontVariants').html(fontVariants);
+				$('#heading').css({'font-family': fontName }).addClass('fadeIn');
+			}
 		}
 
 		function randomizeParagraph(fontObject2){
-			$('#paragraph').removeClass('loaded');
+
 			fontName2 = fontObject2.family;
 			fontNameForUrl2 = fontName2.replace(/\s+/g, '+');
   		    WebFont.load({
@@ -86,34 +90,36 @@
                      families: [fontNameForUrl2] 
                  } 
 		     }); 
-
-			setTimeout(function(){
-				$('#paragraph').css({'font-family': fontName2 }).addClass('loaded');
-			}, 500)
-			
-			$('#paragraphFontInfo').html('<a target="_blank" href="https://www.google.com/fonts/specimen/'+ fontNameForUrl2 +'">' + fontName2 + '</a>');
-
-			var fontVariants = '';
+  		    var fontVariants = '';
 			for (var i = 0; i < fontObject2.variants.length; i++) {
 				fontVariants += '<span onclick="useThisFontVariant(this, \'paragraph\');">' + fontObject2.variants[i] + '</span>';
 			}
-			$('#paragraphFontVariants').html(fontVariants);
-		}
+			$('.paragraph').removeClass('fadeIn');
+			if (refreshCount != 1){
+				$('.paragraphInfoWrapper.current').addClass('slideDown');
+				$('#paragraphInfo').append(
+					'<div class="paragraphInfoWrapper infoWrapper next">'+
+						'<h2>Paragraph</h2>'+
+						'<div class="divider"></div>'+
+						'<span id="paragraphFontInfo"><a target="_blank" href="https://www.google.com/fonts/specimen/'+ fontNameForUrl2 +'">' + fontName2 + '</a></span>'+
+						'<h3>Font Variants:</h3>'+
+						'<span id="paragraphFontVariants">'+fontVariants+'</span>'+
+						'<button id="randomizeParagraph" class="randomizeBtn" onclick="loadFont(false, true)";><i class="fa fa-refresh"></i></button>'+
+					'</div>'
+					);
 
-		function randomizeFontVariant(element) {
-			if (element == heading) {
-				var fontVariantsQuantity = randomFontObject.variants.length;
-				var fontVariantsArray = randomFontObject.variants;
-				var randomVariant = fontVariantsArray[Math.floor(Math.random() * fontVariantsQuantity)];
-				$('#heading').css('font-weight', randomVariant);
-				alert(randomVariant);	
+				setTimeout(function(){
+					$('.paragraphInfoWrapper.current').remove();
+					$('.paragraphInfoWrapper.next').removeClass('next').addClass('current');
+					$('.paragraph').css({'font-family': fontName2 }).addClass('fadeIn');
+				}, 500)
 			}
 			else {
-				var fontVariants = randomFontObject2.variants.length;
+				$('#paragraphFontInfo').html('<a target="_blank" href="https://www.google.com/fonts/specimen/'+ fontNameForUrl2 +'">' + fontName2 + '</a>');
+				$('#paragraphFontVariants').html(fontVariants);
+				$('.paragraph').css({'font-family': fontName2 }).addClass('fadeIn');
 			}
-			
 		}
-
 
 		function useThisFontVariant(variantName, element){
 			var variantName = $(variantName).html();
@@ -131,8 +137,8 @@
 	                   families: [fontNameUrl+':'+variantName] 
 	                } 
 			 });
+
 			//check if font variant name contains just numbers, just letters or both
-			
 
 			var isNumber = /^\d+$/;
 			var isWord = /^\D+$/;
@@ -142,7 +148,7 @@
 					$('#heading').css({'font-weight': variantName, 'font-style':'normal'});
 				}
 				else {
-					$('#paragraph').css({'font-weight': variantName, 'font-style':'normal'});	
+					$('.paragraph').css({'font-weight': variantName, 'font-style':'normal'});	
 				}
 			}
 				//a word
@@ -157,10 +163,10 @@
 				}
 				else {
 					if (variantName == 'regular') {
-						$('#paragraph').css({'font-weight': 400, 'font-style':'normal'});
+						$('.paragraph').css({'font-weight': 400, 'font-style':'normal'});
 					}
 					if (variantName == 'italic') {
-						$('#paragraph').css({'font-style': 'italic', 'font-weight': 400});
+						$('.paragraph').css({'font-style': 'italic', 'font-weight': 400});
 					}
 				}
 
@@ -174,7 +180,7 @@
 				}
 				else {
 					var weight = variantName.substring(0, 3);
-					$('#paragraph').css({'font-style': 'italic', 'font-weight': weight});
+					$('.paragraph').css({'font-style': 'italic', 'font-weight': weight});
 				}
 			}
 
